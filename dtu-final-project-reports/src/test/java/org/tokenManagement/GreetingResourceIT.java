28fa5b1d6152d605ea452ca48abdc59924dd2366
Tama Sarker <33072138+tamasarker1434@users.noreply.github.com>
2024-01-10 18:17:44 +0100

    Initial Comment

---
+++ dtu-final-project-reports/src/test/java/org/tokenManagement/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.tokenManagement;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
